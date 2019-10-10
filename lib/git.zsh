3e21feeabcfdb0a4e22ba58ebee1613a6f0ecdbc
Johnson Zhang <johnson9009@163.com>
2019-10-10 10:35:56 +0800

    Show "${ZSH_THEME_GIT_PROMPT}" When "oh-my-zsh.hide-dirty" == "1"
    Original theme won't show "" when the git repository is clean, so for them just
    show "${ZSH_THEME_GIT_PROMPT_CLEAN}" is ok when "oh-my-zsh.hide-dirty" == "1".
    Here we add one more environment variable "${ZSH_THEME_GIT_PROMPT}" to represent
    the git prompt without dirty information.

--- lib/git.zsh
+++ lib/git.zsh
@@ -28,11 +28,13 @@ function parse_git_dirty() {
         ;;
     esac
     STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
-  fi
-  if [[ -n $STATUS ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    if [[ -n $STATUS ]]; then
+      echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    else
+      echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+    fi
   else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+    echo "${ZSH_THEME_GIT_PROMPT-${ZSH_THEME_GIT_PROMPT_CLEAN}}"
   fi
 }
 
--- themes/robbyrussell.zsh-theme
+++ themes/robbyrussell.zsh-theme
@@ -3,5 +3,6 @@ PROMPT+=' $(git_prompt_info)%{$fg_bold[magenta]%}%c%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[cyan]%}git:(%{$fg_bold[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT="%{$fg_bold[cyan]%})"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[yellow]%}✗%{$fg_bold[cyan]%})"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔%{$fg_bold[cyan]%})"
